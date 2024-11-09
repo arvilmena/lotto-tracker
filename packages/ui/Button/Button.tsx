b23a6d23e92ec14fc1aaa18c8dc7680a841296d4
Arvil Meña <arvilmena@gmail.com>
2024-11-10 06:35:20 +0800

    eslint, pretter, husky, lint-staged

--- packages/ui/Button/Button.tsx
+++ packages/ui/Button/Button.tsx
@@ -1,6 +1,6 @@
 export function Button() {
   return (
-    <button style={{ border: "1px solid red", padding: "3px" }}>
+    <button style={{ border: '1px solid red', padding: '3px' }}>
       This is a button
     </button>
   );
