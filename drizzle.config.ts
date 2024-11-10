c446255147416bfffb55466cd8b3abb9b987fd6c
Arvil Meña <arvilmena@gmail.com>
2024-11-10 08:34:04 +0800

    drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'drizzle-kit';
+export default defineConfig({
+  out: './packages/drizzle/migrations',
+  schema: './packages/drizzle/lib/schema.ts',
+  dialect: 'sqlite',
+  dbCredentials: {
+    url: process.env.DB_FILE_NAME!,
+  },
+});
---
+++ packages/drizzle/lib/db.ts
@@ -0,0 +1,5 @@
+import { Database } from 'bun:sqlite';
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/bun-sqlite';
+const sqlite = new Database(process.env.DB_FILE_NAME!);
+export const db = drizzle({ client: sqlite });
